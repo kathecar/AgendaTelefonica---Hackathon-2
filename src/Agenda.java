9dcd2f795b54dc58c8266c4ed00166533a80798e
Diana1Herrera <dianis11_3@hotmail.com>
2025-02-04 10:32:39 -0500

    eeee

---
+++ src/Agenda.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Agenda extends Contacto{
+
+    ArrayList<Contacto> agenda = new ArrayList<>();
+
+
+
+    public Agenda(String nombre, String apellido, String telefono, ArrayList<Contacto> agenda) {
+        super(nombre, apellido, telefono);
+        this.agenda = agenda;
+    }
+
+   // agenda.add=("ana");
+
+   public void anadirContacto(){
+    // Kate
+
+   }
+    public void existeContacto(){
+    // Kate
+
+    }
+    public void listarContacto(){
+    //Juan
+    }
+    public void buscarContacto(){
+        //Juan
+    }
+    public void eliminarContacto(){
+        //Amalia
+    }
+    public void modificarContacto(){
+        //Amalia
+    }
+    public void agendaLlena(){
+        //Brandon
+
+    }
+    public void espaciosLibres(){
+    //Brandon
+    }
+}
