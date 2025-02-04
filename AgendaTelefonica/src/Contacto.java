0a8cbc6134a6a23f29fb2dfc2b901e6bbfedde50
Diana1Herrera <dianis11_3@hotmail.com>
2025-02-04 10:31:34 -0500

    Repo creada

---
+++ AgendaTelefonica/src/Contacto.java
@@ -0,0 +1,40 @@
+public class Contacto {
+
+    private String nombre, apellido, telefono;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellido, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public String getNonbre() {
+        return nombre;
+    }
+
+    public void setNonbre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+
+}
